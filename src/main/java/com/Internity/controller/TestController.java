6b22d907912f38585abbf9769e1e131e3bcf0e9c
jugalnagar <jugalnagar0@gmail.com>
2022-02-23 18:19:38 +0530

    test / end point

---
+++ src/main/java/com/Internity/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.Internity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @GetMapping
+    public String check() {
+        return  "Testing !!!!!";
+    }
+}
